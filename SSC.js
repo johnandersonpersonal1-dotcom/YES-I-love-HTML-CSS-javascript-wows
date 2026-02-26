24a97145eb41fc5df2b18a4b58aa66fafaf34445
johnandersonpersonal1-dotcom <johnandersonpersonal1@gmail.com>
2026-02-26 22:05:11 +0600

    Add files via upload

---
+++ SSC.js
@@ -0,0 +1,29 @@
+function validate(e){
+    e.preventDefault()
+
+    const email = document.getElementById('email').value;
+    const pass = document.getElementById('password').value;
+    const age = document.getElementById('age').value;
+    const msgBox = document.getElementById('message');
+
+    let message = '';
+    if(email === ''){
+        message = "Please Enter an Email.";
+        msgBox.style.color = "Light Red";
+    } else if(pass === ''){
+       message = "Please Enter a Password.";
+       msgBox.style.color = "Light Red" ;
+    } else if(age === ''){
+       message = "Please Enter your age.";
+       msgBox.style.color = "Light Red" ;
+    } else{
+       message = "Login Successful.";
+       msgBox.style.color = "Light Blue" ;
+    }
+    msgBox.innerHTML = message;
+}
+
+document.getElementById("loginForm").innerHTML = validate;
+document.getElementById("email").oninput = () => validate({ preventDefault: () => {}});
+document.getElementById("password").oninput = () => validate({ preventDefault: () => {}});
+document.getElementById("age").oninput = () => validate({ preventDefault: () => {}});
